0cef332fe4e6a0b092d72b84a8ce4b2268152896
Rodrigolu1zz <rodrigoluiz924@gmail.com>
2025-04-05 09:56:02 -0300

    Adicionado um calculo e condicional na main

--- Teste/src/App.java
+++ Teste/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual o resultado da expressão (2 * 5) + (10 / 2)?");
+
+        short resultado = 0;
+        resultado = sc.nextShort();
+
+        if (resultado ==  (2 * 5) + (10 / 2)){ //Aqui pode substituir (2 * 5) + (10 / 2) por 15 que seria a resposta
+            System.out.println("Correto!");
+        } else {
+            System.out.println("Errado!");
+        }
+
+        sc.close();
     }
 }
